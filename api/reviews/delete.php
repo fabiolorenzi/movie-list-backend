ddaefa72c792c1108b929106047f56d37e0cc0a8
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-03-31 23:18:17 +0100

    developed code to delete review

---
+++ api/reviews/delete.php
@@ -0,0 +1,26 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Methods: DELETE");
+    header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
+  
+    include_once "../../config/Database.php";
+    include_once "../../models/Review.php";
+  
+    $database = new Database();
+    $db = $database->connect();
+  
+    $review = new Review($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+    $review->id = $data->id;
+
+    if ($review->delete()) {
+        echo json_encode(
+            array("message" => "Review deleted successfully")
+        );
+    } else {
+        echo json_encode(
+            array("message" => "Review not deleted corrently")
+        );
+    };
