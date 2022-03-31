5be45fce96d5ee9223e5ca86fccf0d101b4911de
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-03-31 16:40:34 +0100

    developed basic movie model with read movies code

---
+++ models/Movie.php
@@ -0,0 +1,31 @@
+<?php
+    class Movie {
+        private $conn;
+        private $table = "movies";
+
+        public $id;
+        public $title;
+        public $genre;
+        public $year;
+        public $desc;
+        public $img;
+        public $created_at;
+
+
+        // To connect to the db
+        public function _construct($db) {
+            $this->conn = $db;
+        }
+
+        // Get single movie
+
+        public function read() {
+            $query = "SELECT m.id, m.title, m.genre, m.year, m.desc, m.img, m.created_at
+                FROM " . $this->table . " m";
+
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            
+            return $stmt;
+        }
+    }
