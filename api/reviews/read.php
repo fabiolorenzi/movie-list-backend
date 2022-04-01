e10d63ee150b5a1f66b13072e60ae5255e100a73
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-04-01 11:28:44 +0100

    improve cors policy

--- api/reviews/read.php
+++ api/reviews/read.php
@@ -1,6 +1,8 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json");
+    header("Access-Control-Allow-Methods: GET");
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
     
     include_once "../../models/Review.php";
     include_once "../../config/Database.php";
